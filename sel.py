2e73278b7a287b18a0dfe2853d62f36993689f6d
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-06-04 09:07:06 +0200

    print error message if bing doesnt find anything

--- sel.py
+++ sel.py
@@ -1,7 +1,22 @@
+"""Usage: pull_updates [-hbg] SITE
+
+Use google or bing to search for most recent changes on a website
+
+Arguments:
+  SITE      website to search for
+
+Options:
+  -h --help
+  -b          use Bing
+  -g          use Google    
+"""
+
 import os
+import sys
+import time
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium import webdriver
-import pathlib
+# import pathlib
 
 import selenium.webdriver
 import selenium.webdriver.support.ui
@@ -9,7 +24,7 @@ import selenium.webdriver.support.ui
 import selenium.webdriver.support.wait
 from selenium.webdriver.support import expected_conditions as EC
 
-import time
+from docopt import docopt
 
 # prof = selenium.webdriver.FirefoxProfile()
 opt = selenium.webdriver.FirefoxOptions()
@@ -25,7 +40,7 @@ driver_wait = selenium.webdriver.support.wait.WebDriverWait(driver, 30)
 def scroll_click(xpath):
     element = driver.find_element_by_xpath(xpath)
     driver.execute_script("return arguments[0].scrollIntoView();", element)
-    time.sleep(1)
+    time.sleep(.3)
     element.click()
     return element
 
@@ -74,20 +89,28 @@ def google(term):
     if '' != driver.find_element_by_xpath('//div[@id="topstuff"]').text:
         driver.get('https://www.google.com/search?q=' + term +
                    '&tbs=cdr:1,cd_min:2000,cd_max:2018,sbd:1')
+    # first link goes to google but has full url encoded
+    link = '//h3[@class="r"]/a'
+    return [e.get_attribute('href')
+            for e in driver.find_elements_by_xpath(link)]
 
 
 # https://github.com/azzamsa/awesome-lisp-companies
 # https://github.com/azzamsa/awesome-cl-software
 
-def bing(term):
-    name = 'site:' + term
-    for i in ['1', '2', '3', '5_0_24000']:
+def bing(name):
+    #name = 'site:' + term
+    for i in ['1', '2', '3', '5_0_2400x0']:
         driver.get('https://www.bing.com/search?q=' +
                    name + '&filters=ex1:"ez' + i + '"')
         try:
             el = driver.find_element_by_xpath('//li[@class="b_no"]')
         except selenium.common.exceptions.NoSuchElementException:
-            return el
+            link = '//ol/li/h2/a'
+            # time.sleep(1)
+            wait(link)
+            return [e.get_attribute('href')
+                    for e in driver.find_elements_by_xpath(link)]
 
 
 url = ['ampleon.com', 'cavendish-kinetics.com',
@@ -180,68 +203,36 @@ crunchbase = ['crunchbase.com',
               'dutchstartupmap.com',
               'startupnorway.com', 'swissstartups.org']
 
-bing(url[-23])
 
-[e.get_attribute('href')
- for e in driver.find_elements_by_xpath('//ol/li/h2/a')]
+#bing('site:' + crunchbase[23] + ' photonics')
+#google('site:' + url[-1])
 
-bing(crunchbase[3] + ' photonics')
-google('site:' + url[-1])
-google('site:digg.com')
+def marktplaats(term):
+    # accept cookies
+    driver.find_element_by_xpath(
+        '/html/body/div/div/div[2]/form/input[2]').click()
 
+    inpt = driver.find_element_by_xpath('//*[@id="input"]')
+    inpt.clear()
+    inpt.send_keys(term + '\n')
 
-url = driver.find_element_by_xpath('//cite[@class="iUh30"]')
+    postcode = driver.find_element_by_xpath('//input[@name="postcode"]')
+    postcode.clear()
+    postcode.send_keys(u'9905')
 
-driver.get(url.text)
-
-for url in urls:
-    print(url)
-
-
-driver.get('https://blog.fefe.de')
-driver.get('https://reddit.com')
-driver.get('https://news.ycombinator.com')
-driver.get('https://marktplaats.nl')
-
-
-# accept cookies
-driver.find_element_by_xpath('/html/body/div/div/div[2]/form/input[2]').click()
-
-inpt = driver.find_element_by_xpath('//*[@id="input"]')
-inpt.clear()
-inpt.send_keys(u'computer\ue007')
-
-postcode = driver.find_element_by_xpath('//input[@name="postcode"]')
-postcode.clear()
-postcode.send_keys(u'9905')
-
-submit = driver.find_element_by_xpath('//button[@type="submit"]')
-submit.click()
-# path to the firefox binary inside the Tor package
-
-binary = '/dev/shm/'
-if os.path.exists(binary) is False:
-    raise ValueError("The binary path to Tor firefox does not exist.")
-firefox_binary = FirefoxBinary(binary)
-
-
-browser = None
-
-
-def get_browser(binary=None):
-    global browser
-    # only one instance of a browser opens, remove global for multiple instances
-    if not browser:
-        browser = webdriver.Firefox(firefox_binary=binary)
-    return browser
+    submit = driver.find_element_by_xpath('//button[@type="submit"]')
+    submit.click()
 
 
 if __name__ == "__main__":
-    browser = get_browser(binary=firefox_binary)
-    urls = (
-        ('tor browser check', 'https://check.torproject.org/'),
-        ('ip checker', 'http://icanhazip.com')
-    )
-    for url_name, url in urls:
-        print("getting", url_name, "at", url)
-        browser.get(url)
+    arguments = docopt(__doc__)
+    if arguments['-b']:
+        try:
+            for x in bing('site:' + arguments['SITE']):
+                print(x)
+        except TypeError:
+            print('no results', file=sys.stderr)
+    if arguments['-g']:
+        for x in google('site:' + arguments['SITE']):
+            print(x)
+    driver.quit()
